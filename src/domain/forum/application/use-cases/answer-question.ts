713f020b34bae0bc77f8f4f87cd7d6a51e0975df
gaiake12 <gaiake12@hotmail.com>
2025-04-28 18:30:32 -0300

    refactor: reorganize folder and archives

--- src/domain/forum/application/use-cases/answer-question.ts
+++ src/domain/forum/application/use-cases/answer-question.ts
@@ -1,6 +1,6 @@
 import { UniqueEntityID } from '../../core/entities/unique-entity-id'
-import { Answer } from '@/domain/forum/enterprise/entities/answer'
-import { AnswersRepository } from '../forum/application/repositories/answers-repository'
+import { Answer } from '../entities/answer'
+import { AnswersRepository } from '../repositories/answers-repository'
 
 interface AnswerQuestionUseCaseRequest {
   instructorId: string
