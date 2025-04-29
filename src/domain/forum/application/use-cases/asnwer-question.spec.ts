f559bf8feeb8862f6ae8f66558837b67f8e87a14
gaiake12 <gaiake12@hotmail.com>
2025-04-29 11:10:58 -0300

    refactor: rewriting tests and reorganizing tests repositories

--- src/domain/forum/application/use-cases/asnwer-question.spec.ts
+++ src/domain/forum/application/use-cases/asnwer-question.spec.ts
@@ -1,18 +1,22 @@
 import { AnswerQuestionUseCase } from './answer-question'
-import { AnswersRepository } from '../repositories/answers-repository'
+import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
 
-const fakeAnswersRepository: AnswersRepository = {
-  create: async () => {},
-}
+let inMemoryAnswersRepository: InMemoryAnswersRepository
+let sut: AnswerQuestionUseCase
 
-test('create answer', async () => {
-  const answerQuestionUseCase = new AnswerQuestionUseCase(fakeAnswersRepository)
-
-  const answer = await answerQuestionUseCase.execute({
-    content: 'Nova resposta',
-    instructorId: '1',
-    questionId: '2',
+describe('Answer Question', () => {
+  beforeEach(() => {
+    inMemoryAnswersRepository = new InMemoryAnswersRepository()
+    sut = new AnswerQuestionUseCase(inMemoryAnswersRepository)
   })
+  it('should be able to create an answer', async () => {
+    const { answer } = await sut.execute({
+      content: 'Nova resposta',
+      instructorId: '1',
+      questionId: '2',
+    })
 
-  expect(answer.content).toEqual('Nova resposta')
+    expect(answer.content).toEqual('Nova resposta')
+    expect(inMemoryAnswersRepository.items[0].id).toEqual(answer.id)
+  })
 })
