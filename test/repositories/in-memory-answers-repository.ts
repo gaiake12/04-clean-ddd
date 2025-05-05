051e9291d7ce86277541410fa027572020dbf0f6
gaiake12 <gaiake12@hotmail.com>
2025-05-05 17:52:28 -0300

    feat: implementing delete answer and tests

--- test/repositories/in-memory-answers-repository.ts
+++ test/repositories/in-memory-answers-repository.ts
@@ -4,6 +4,22 @@ import { Answer } from '@/domain/forum/enterprise/entities/answer'
 export class InMemoryAnswersRepository implements AnswersRepository {
   public items: Answer[] = []
 
+  async findById(id: string) {
+    const answer = this.items.find((item) => item.id.toString() === id)
+
+    if (!answer) {
+      return null
+    }
+
+    return answer
+  }
+
+  async delete(answer: Answer) {
+    const answerIndex = this.items.findIndex((item) => item.id === answer.id)
+
+    this.items.splice(answerIndex, 1)
+  }
+
   async create(answer: Answer) {
     this.items.push(answer)
   }
