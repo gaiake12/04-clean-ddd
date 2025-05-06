6e070689f0985f1ff3522638dd542305673aa9c1
gaiake12 <gaiake12@hotmail.com>
2025-05-06 10:58:15 -0300

    feat: implementing fetch recent questions and its tests

--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -1,22 +1,9 @@
+import { PaginationParams } from '@/core/repositories/pagination-params'
 import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
 import { Question } from '@/domain/forum/enterprise/entities/question'
 
 export class InMemoryQuestionsRepository implements QuestionsRepository {
   public items: Question[] = []
-
-  async findById(id: string) {
-    const question = this.items.find((item) => {
-      console.log(item.id.toString())
-
-      return item.id.toString() === id
-    })
-
-    if (!question) {
-      return null
-    }
-
-    return question
-  }
 
   async delete(question: Question) {
     const questionIndex = this.items.findIndex(
@@ -50,5 +37,27 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
     }
 
     return question
+  }
+
+  async findById(id: string) {
+    const question = this.items.find((item) => {
+      console.log(item.id.toString())
+
+      return item.id.toString() === id
+    })
+
+    if (!question) {
+      return null
+    }
+
+    return question
+  }
+
+  async findManyRecent({ page }: PaginationParams) {
+    const questions = this.items
+      .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
+      .slice((page - 1) * 20, page * 20)
+
+    return questions
   }
 }
