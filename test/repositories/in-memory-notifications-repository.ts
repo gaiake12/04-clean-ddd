e6d430401c8b937bf77f0955a299c656ebb2d787
gaiake12 <gaiake12@hotmail.com>
2025-06-11 16:59:15 -0300

    feat: read notification and reorganizing some error files

--- test/repositories/in-memory-notifications-repository.ts
+++ test/repositories/in-memory-notifications-repository.ts
@@ -6,6 +6,24 @@ export class InMemoryNotificationsRepository
 {
   public items: Notification[] = []
 
+  async findById(id: string): Promise<Notification | null> {
+    const notification = this.items.find((item) => item.id.toString() === id)
+
+    if (!notification) {
+      return null
+    }
+
+    return notification
+  }
+
+  async save(notification: Notification) {
+    const notificationIndex = this.items.findIndex(
+      (item) => item.id === notification.id,
+    )
+
+    this.items[notificationIndex] = notification
+  }
+
   async create(notification: Notification) {
     this.items.push(notification)
   }
